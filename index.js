bb2d4c484848ee2c2064faf09ddaada7f2513837
mutembeic <135004069+mutembeic@users.noreply.github.com>
2023-06-14 06:10:21 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(_distance){
+  if (_distance <= 400) {
+    return 'This one is on me!';
+  } else if (_distance > 2500) {
+    return 'No can do.';
+  } else {
+    return 'I will gladly take your thirty bucks.';
+  }
+}
+  // Write your code  here!
+
+
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  } else {
+    return "No go."
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
   // Write your code here!
 }
